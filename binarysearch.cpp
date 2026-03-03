b1c13fb8e54196b35cac121968dc961bed6da5dd
NguyenHaiAnh06 <tqx4nb4j7r@privaterelay.appleid.com>
2026-03-03 16:06:19 +0700

    binary

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -1,37 +1,48 @@
-#include<iostream>
-#include<cstdio>
-using namespace std;
-int binarysearch(int a[],int x,int left,int right){
-    while (left<=right)
+#include <stdio.h>
+int binarysearch(int a[], int x, int left, int right)
+{
+    while (left <= right)
     {
-        int mid=(left+right)/2;
-        if(a[mid]==x){
+        int mid = (left + right) / 2;
+        if (a[mid] == x)
             return 1;
-        } else if (x>a[mid])
+        else if (x > a[mid])
         {
-            left=mid+1;
-        } else{
-            right=mid-1;
+            left = mid + 1;
         }
-        
+        else
+        {
+            right = mid - 1;
+        }
     }
     return -1;
 }
 
-int main(){
-int n,x;
-cin >>n;
-int a[n];
-for (int i = 0; i < n; i++)
+int main()
 {
-    cin>>a[i];
-}
-    cin>>x;
-    int res=binarysearch(a,x,0,n-1);
-    if(res==1){
-    cout<<"YES\n"<< endl;
-    } else{
-        cout<<"NO\n"<<endl;
+    int n, x;
+    printf("nhap kich thuoc mang: ");
+    scanf("%d", &n);
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
     }
-return 0;
+    int t; // nhap so luong can tim
+
+    scanf("%d", &t);
+    while (t--)
+    {
+        scanf("%d", &x);
+        int res = binarysearch(a, x, 0, n - 1);
+        if (res == 1)
+        {
+            printf("yes\n");
+        }
+        else
+        {
+            printf("No\n");
+        }
+    }
+    return 0;
 }
