c066089d42d4ec81a344bc0bba030fd57c701062
NguyenHaiAnh06 <tqx4nb4j7r@privaterelay.appleid.com>
2026-07-08 22:48:38 +0700

    messi

---
+++ gpt_luyentap1.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <cmath>
+#include <numeric>
+#include <iomanip>
+#include <limits>
+
+using namespace std;
+
+#define FOR2_1(i, j, row, col)       \
+    for (int i = 1; i <= (row); ++i) \
+        for (int j = 1; j <= (col); ++j)
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    ll q;
+    cin >> q;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    ll f[n];
+    for (int i = 0; i < n; i++)
+    {
+        if (i == 0)
+        {
+            f[i] = a[i];
+        }
+        else
+        {
+            f[i] = f[i - 1] + a[i];
+        }
+    }
+    while (q--)
+    {
+        int left, right;
+        cin >> left >> right;
+        left--, right--;
+        if (left == 0)
+        {
+            cout << f[right] << endl;
+        }
+        else
+        {
+            cout << f[right] - f[left - 1] << endl;
+        }
+    }
+
+    return 0;
+}
